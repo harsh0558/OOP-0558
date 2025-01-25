960d2024f81891e99f2d2a0457a6eed9b274db1d
Harsh Maghnani <hmaghnani6@gmail.com>
2025-01-25 14:37:40 +0500

    finalized

--- Lab 1/In-Lab Tasks/Tasks.cpp
+++ Lab 1/In-Lab Tasks/Tasks.cpp
@@ -2,97 +2,105 @@
 
 using namespace std;
 
-int main(){
-
+int main() {
     // Lab task 1
-
     string event1[5];
     string event2[5];
+    int count1 = 0, count2 = 0;
+    char choice;
 
-    for(int i=0;i<5;i++){
-        cout << "enter the name of the participant of event 1: ";
-        cin >> event1[i];
-
-        cout << "enter the name of the participant of event 2 : ";
-        cin >> event2[i];
+    cout << "Register participants for Event 1:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Do you want to enter a participant for Event 1? (y/n): ";
+        cin >> choice;
+        if (choice == 'n' || choice == 'N') break;
+        cout << "Enter the name of the participant: ";
+        cin >> event1[count1++];
     }
 
-    // lab task 2
-    bool isPresent;
+    cout << "\nRegister participants for Event 2:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Do you want to enter a participant for Event 2? (y/n): ";
+        cin >> choice;
+        if (choice == 'n' || choice == 'N') break;
+        cout << "Enter the name of the participant: ";
+        cin >> event2[count2++];
+    }
 
+    // Lab task 2
+    bool isPresent = false;
     string name;
 
-    cout<< "enter the name of the participant: ";
-    cin>>name;
+    cout << "\nEnter the name of the participant to check: ";
+    cin >> name;
 
-
-    for(int i=0;i<5;i++){
-        if(name==event1[i] || name==event2[i]){
-            isPresent =true;
+    for (int i = 0; i < count1; i++) {
+        if (name == event1[i]) {
+            isPresent = true;
+            break;
+        }
+    }
+    for (int i = 0; i < count2; i++) {
+        if (name == event2[i]) {
+            isPresent = true;
             break;
         }
     }
 
-    if(isPresent==true){
-        cout<<"the participant entered is present"<<endl;
-    }else{
-        cout<<"the participant entered is not present"<<endl;
+    if (isPresent) {
+        cout << "The participant entered is present" << endl;
+    } else {
+        cout << "The participant entered is not present" << endl;
     }
 
-    // lab task 3
-
-    int total=0;
+    // Lab task 3
+    int total = 0;
     int num;
-    cout<<"FOR EVENT 1"<< endl;
+    cout << "\nFOR EVENT 1" << endl;
 
-    for(int i=0;i<5;i++){
-        cout<<"enter the amount donated by "<<event1[i]<<": ";
-        cin>>num;
-        total +=num+10;
+    for (int i = 0; i < count1; i++) {
+        cout << "Enter the amount donated by " << event1[i] << ": ";
+        cin >> num;
+        total += num;
     }
 
-    cout<< "total donations from event 1: "<<total <<endl;
+    cout << "Total donations from Event 1: " << total << endl;
 
-    total=0;
-    cout<<"FOR EVENT 2"<< endl;
+    total = 0;
+    cout << "\nFOR EVENT 2" << endl;
 
-    for(int i=0;i<5;i++){
-        cout<<"enter the amount donated by "<<event2[i]<<": ";
-        cin>>num;
-        total +=num+10;
+    for (int i = 0; i < count2; i++) {
+        cout << "Enter the amount donated by " << event2[i] << ": ";
+        cin >> num;
+        total += num;
     }
 
-    cout<< "total donations from event 2: "<<total<< endl;
+    cout << "Total donations from Event 2: " << total << endl;
 
-    // lab task 4
-
-    cout<<"participants of event 1 (reversed)"<<endl;
-
-    string revString1[5];
-    string revString2[5];
-
-    for(int i=0;i<5;i++){
-        int length = event1[i].length();
-        int rev = length-1;
-        string temp = "";
-        for(int j=0;j<length;j++){
-            temp = temp + event1[i][rev];
-            rev--;
-        }
-         revString1[i] =temp;
-        cout<<"participant "<< i+1 <<":"<< revString1[i]<<endl;
+    // Lab task 4
+    cout << "\nParticipants of Event 1 (reversed)" << endl;
+    for (int i = count1 - 1; i >= 0; i--) {
+        cout << "Participant " << (count1 - i) << ": " << event1[i] << endl;
     }
 
-    cout<<"participants of event 2 (reversed)"<<endl;
-    for(int i=0;i<5;i++){
-        int length = event2[i].length();
-        int rev = length-1;
-        string temp = "";
-        for(int j=0;j<length;j++){
-            temp = temp + event2[i][rev];
-            rev--;
-        }
-         revString1[i] =temp;
-        cout<<"participant "<< i+1 <<":"<< revString1[i]<<endl;
+    cout << "\nParticipants of Event 2 (reversed)" << endl;
+    for (int i = count2 - 1; i >= 0; i--) {
+        cout << "Participant " << (count2 - i) << ": " << event2[i] << endl;
     }
+
+    // Lab task 5: Event Popularity Bar Chart
+    cout << "\nEvent Popularity Bar Chart" << endl;
+    cout << "Event 1: ";
+    for (int i = 0; i < count1; i++) {
+        cout << "*";
+    }
+    cout << " (" << count1 << " participants)" << endl;
+
+    cout << "Event 2: ";
+    for (int i = 0; i < count2; i++) {
+        cout << "*";
+    }
+    cout << " (" << count2 << " participants)" << endl;
+
+    return 0;
 }
